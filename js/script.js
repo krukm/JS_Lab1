c67cc99f98dca1b64ddbdb49145370890e9f172a
Matthew Kruk <matthew.kruk@gmail.com>
2018-07-24 16:26:30 -0400

    refactor to encapsulate code into functions

--- js/script.js
+++ js/script.js
@@ -1,36 +1,49 @@
-let turn = 1;
 let win = 0;
 let userHp = 40;
 let grantHp = 10;
-let play = prompt("Do you want to play?");
+let name;
 
-if (play.toLowerCase() === 'yes') {
-    let name = prompt("Enter your name:");
+startGame();
 
+function startCombat() {
     while (userHp > 0 && win !== 3) {
-        switch(turn) {
-            case 1:
-                userHp -= (Math.floor(Math.random() * 2) + 1);
-                console.log(`${name} has ${userHp} health left.`);
-                turn = 2;
-                break;
-            case 2:
-                grantHp -= (Math.floor(Math.random() * 2) + 1);
-                console.log(`Grant the Mighty Chicken has ${grantHp} health left`);
-                if (grantHp < 1) {
-                    win++;
-                    grantHp = 10;
-                }
-                turn = 1;
-                break;
+        
+    let action = prompt("Would you like to attack(a) or quit(q)?");
+
+    if (action == "a") {
+        userHp -= getDamage();
+        console.log(`${name} has ${userHp} health left.`);
+    } else if (action == "q") {
+        console.log(`You chose to run away with your ${userHp} remaining health...`);
+        break;
+    } 
+    grantHp -= getDamage();
+    console.log(`Grant the Mighty Chicken has ${grantHp} health left`);
+    if (userHp < 0) {
+        console.log(`Oh Noes!!! Grant the Mighty Chicken destroyed you...`);
+    } else if (grantHp < 1) {
+        win++;
+        grantHp = 10;
+            if (win < 3) {
+                console.log(`Congrats ${name}, you whooped on Grant real good! Uh-oh Grant used a health potion, beat him ${3 - win} time(s) to win.`)
+            } else {
+                console.log(`Nicely done, ${name}! You defeated Grant the Mighty Chicken!!!`);
+            }
         }
     }
+}
 
-    if (win === 3) {
-        console.log("You win!!!")
+function startGame() {
+    let play = prompt("Do you want to play?");
+
+    if (play.toLowerCase() === 'yes') {
+        name = prompt("Enter your name:");
+        startCombat();
     } else {
-        console.log("Grant wins...")
+        console.log("See ya later!");
     }
-} else {
-    console.log("See ya later!");
+}
+
+function getDamage() {
+    return (Math.floor(Math.random() * 5) + 1);
 }
